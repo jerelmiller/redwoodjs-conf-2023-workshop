31e97d3e1aafddcb270163e7d43cf406b540f524
Jerel Miller <jerelmiller@gmail.com>
2023-09-07 17:55:54 -0600

    Bootstrap a SidebarPlaylistsCell

---
+++ web/src/components/SidebarPlaylistsCell/SidebarPlaylistsCell.tsx
@@ -0,0 +1,30 @@
+import type { SidebarPlaylistsQuery } from 'types/graphql'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query SidebarPlaylistsQuery {
+    sidebarPlaylists {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  sidebarPlaylists,
+}: CellSuccessProps<SidebarPlaylistsQuery>) => {
+  return (
+    <ul>
+      {sidebarPlaylists.map((item) => {
+        return <li key={item.id}>{JSON.stringify(item)}</li>
+      })}
+    </ul>
+  )
+}
