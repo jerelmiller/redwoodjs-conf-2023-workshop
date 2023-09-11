091b1c9a2ade1dae3edd85a9e96a9c8673a7274c
Jerel Miller <jerelmiller@gmail.com>
2023-09-11 12:23:49 -0600

    Add a working (but not type-safe) implementation of sidebar playlists

--- web/src/components/SidebarPlaylistsCell/SidebarPlaylistsCell.tsx
+++ web/src/components/SidebarPlaylistsCell/SidebarPlaylistsCell.tsx
@@ -1,6 +1,13 @@
+import cx from 'classnames'
+import { Pin, Volume2 } from 'lucide-react'
 import type { SidebarPlaylistsQuery } from 'types/graphql'
 
+import { NavLink } from '@redwoodjs/router'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+import CoverPhoto from 'src/components/CoverPhoto'
+import DelimitedList from 'src/components/DelimitedList'
+import { thumbnail } from 'src/utils/image'
 
 export const QUERY = gql`
   query SidebarPlaylistsQuery {
@@ -26,10 +33,55 @@ export const Failure = ({ error }: CellFailureProps) => (
 )
 
 export const Success = ({ me }: CellSuccessProps<SidebarPlaylistsQuery>) => {
+  const isCurrentContext = false
+  const pinned = false
+  const playbackState = { isPlaying: false }
+
   return (
     <ul>
-      {me.playlists?.edges?.map(({ node }) => {
-        return <li key={node.id}>{JSON.stringify(node)}</li>
+      {me.playlists?.edges?.map(({ node: playlist }) => {
+        return (
+          <li key={playlist.id}>
+            <NavLink
+              to={`/playlists/${playlist.id}`}
+              className="transition-color block justify-between rounded-md py-2 pl-2 pr-4 leading-none transition-colors duration-200 ease-out hover:bg-[#1A1A1A] hover:no-underline"
+              activeClassName="bg-surface text-primary hover:bg-[#393939]"
+            >
+              <div className="flex items-center gap-3">
+                <CoverPhoto image={thumbnail(playlist.images ?? [])} />
+                <div className="flex flex-1 flex-col justify-around self-stretch overflow-hidden text-ellipsis whitespace-nowrap">
+                  <div
+                    className={cx(
+                      'overflow-hidden text-ellipsis whitespace-nowrap',
+                      { 'text-theme-light': isCurrentContext }
+                    )}
+                  >
+                    {playlist.name}
+                  </div>
+                  <div className="flex items-center gap-2">
+                    {pinned && (
+                      <Pin
+                        fill="currentColor"
+                        size="1rem"
+                        strokeWidth={1}
+                        className="rotate-45 text-theme-light"
+                      />
+                    )}
+                    <span className="text-sm text-muted">
+                      <DelimitedList delimiter=" · ">
+                        <span>Playlist</span>
+                        <span>{playlist.owner?.displayName}</span>
+                      </DelimitedList>
+                    </span>
+                  </div>
+                </div>
+                {isCurrentContext && playbackState?.isPlaying && (
+                  <Volume2 color="var(--color--theme--light)" size="0.875rem" />
+                )}
+              </div>
+            </NavLink>
+          </li>
+        )
       })}
     </ul>
   )
