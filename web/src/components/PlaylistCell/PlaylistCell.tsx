2ac94333bbd0b05d985e5ca3cfaf8af025b9f0c4
Jerel Miller <jerelmiller@gmail.com>
2023-09-11 17:36:31 -0600

    Add page cover photo to PlaylistCell

--- web/src/components/PlaylistCell/PlaylistCell.tsx
+++ web/src/components/PlaylistCell/PlaylistCell.tsx
@@ -5,15 +5,20 @@ import type {
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 import PageContainer from 'src/components/PageContainer'
+import PageCoverPhoto from 'src/components/PageCoverPhoto'
 import PageContent from 'src/components/PageContent'
 import PageHeader from 'src/components/PageHeader'
 import PageTitle from 'src/components/PageTitle'
+import PlayButton from '../PlayButton/PlayButton'
 
 export const QUERY = gql`
   query FindPlaylistQuery($id: ID!) {
     playlist(id: $id) {
       id
       name
+      images {
+        url
+      }
     }
   }
 `
@@ -34,12 +39,13 @@ export const Success = ({
   return (
     <PageContainer>
       <PageHeader>
-        <PageTitle>Playlist</PageTitle>
+        <PageCoverPhoto image={playlist.images[0]} />
+        <PageTitle>{playlist.name}</PageTitle>
       </PageHeader>
       <PageContent>
-        <p>
-          Find me in <code>./web/src/pages/PlaylistPage/PlaylistPage.tsx</code>
-        </p>
+        <div>
+          <PlayButton playing={false} size="3.5rem" variant="primary" />
+        </div>
       </PageContent>
     </PageContainer>
   )
