8fcb2a44f6f0dbcd0045cb0bc7c68302d5e41423
Jerel Miller <jerelmiller@gmail.com>
2023-09-12 16:02:59 -0600

    Add media tile components

---
+++ web/src/components/MediaTileDetails/MediaTileDetails.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react'
+import DelimitedList from 'src/components/DelimitedList'
+
+interface MediaTileDetailsProps {
+  children?: ReactNode
+}
+
+const MediaTileDetails = ({ children }: MediaTileDetailsProps) => {
+  return (
+    <div className="line-clamp-2 text-sm text-muted">
+      <DelimitedList delimiter=" · ">{children}</DelimitedList>
+    </div>
+  )
+}
+
+export default MediaTileDetails
