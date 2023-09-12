17618218d3bdff8612656ad23ba3188e5a48f03b
Jerel Miller <jerelmiller@gmail.com>
2023-09-12 01:43:49 -0600

    Add release date to album

---
+++ api/db/migrations/20230912074341_add_release_date_to_album/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `releaseDate` to the `Album` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `releaseDatePrecision` to the `Album` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Album" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "albumType" TEXT NOT NULL DEFAULT 'album',
+    "releaseDate" DATETIME NOT NULL,
+    "releaseDatePrecision" TEXT NOT NULL
+);
+INSERT INTO "new_Album" ("albumType", "id", "name") SELECT "albumType", "id", "name" FROM "Album";
+DROP TABLE "Album";
+ALTER TABLE "new_Album" RENAME TO "Album";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
