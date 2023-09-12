70fe4b9d947e4e5cd3e52b267a6e2774657e1ec4
Jerel Miller <jerelmiller@gmail.com>
2023-09-12 15:37:39 -0600

    Create a MediaTile component

---
+++ web/src/components/MediaTile/MediaTile.tsx
@@ -0,0 +1,22 @@
+import { ComponentPropsWithoutRef, ReactNode } from 'react'
+import { Link } from '@redwoodjs/router'
+
+type LinkProps = ComponentPropsWithoutRef<typeof Link>
+
+interface MediaTileProps {
+  children?: ReactNode
+  to: LinkProps['to']
+}
+
+const MediaTile = ({ children, to }: MediaTileProps) => {
+  return (
+    <Link
+      to={to}
+      className="flex flex-col gap-4 rounded bg-surface-low-contrast p-4 transition-colors duration-200 ease-out hover:bg-surface-low-contrast-hover hover:no-underline"
+    >
+      {children}
+    </Link>
+  )
+}
+
+export default MediaTile
