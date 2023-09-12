1955c5be2e9b1045a8bd0db8417ce302c91f22b6
Jerel Miller <jerelmiller@gmail.com>
2023-09-12 01:27:19 -0600

    Add ability to query albums on an artist

--- api/src/graphql/artists.sdl.ts
+++ api/src/graphql/artists.sdl.ts
@@ -1,6 +1,28 @@
 export const schema = gql`
   type Artist {
     """
+    Spotify catalog information about an artist's albums.
+    """
+    albums(
+      """
+      Used to filter the response. If not supplied, all album types will be
+      returned.
+      """
+      includeGroups: [AlbumGroup!]
+
+      """
+      The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
+      """
+      limit: Int = 20
+
+      """
+      The index of the first item to return. Default: 0 (the first item). Use
+      with \`limit\` to get the next set of items.
+      """
+      offset: Int = 0
+    ): ArtistAlbumsConnection
+
+    """
     The [Spotify ID](https://developer.spotify.com/documentation/web-api/#spotify-uris-and-ids)
     for the album.
     """
@@ -20,6 +42,25 @@ export const schema = gql`
     The name of the artist.
     """
     name: String!
+  }
+
+  type ArtistAlbumsConnection {
+    """
+    A list of albums that belong to the artist.
+    """
+    edges: [ArtistAlbumEdge!]
+
+    """
+    "Pagination information for the set of albums"
+    """
+    pageInfo: PageInfo!
+  }
+
+  type ArtistAlbumEdge {
+    """
+    Spotify catalog information for the album.
+    """
+    node: Album!
   }
 
   enum AlbumGroup {
--- api/src/services/artists/artists.ts
+++ api/src/services/artists/artists.ts
@@ -9,6 +9,25 @@ export const artist: QueryResolvers['artist'] = ({ id }) => {
 }
 
 export const Artist: ArtistRelationResolvers = {
+  albums: async ({ limit, offset }, { root }) => {
+    const total = await db.album.count({
+      where: { artists: { some: { id: root.id } } },
+    })
+    const albums = await db.artist
+      .findUniqueOrThrow({ where: { id: root.id } })
+      .albums({ take: limit, skip: offset })
+
+    return {
+      pageInfo: {
+        limit,
+        offset,
+        total,
+        hasNextPage: offset + albums.length < total,
+        hasPreviousPage: offset > 0 && total > 0,
+      },
+      edges: albums.map((album) => ({ node: album })),
+    }
+  },
   images: (_, { root }) => {
     return db.artist.findUniqueOrThrow({ where: { id: root.id } }).images()
   },
