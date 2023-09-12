cd236b91997aeba65a2878e5fd194d0016316255
Jerel Miller <jerelmiller@gmail.com>
2023-09-12 01:04:00 -0600

    Add additional fields to artist resolver

--- api/src/graphql/artists.sdl.ts
+++ api/src/graphql/artists.sdl.ts
@@ -1,11 +1,35 @@
 export const schema = gql`
   type Artist {
-    id: String!
-    name: String!
+    """
+    The [Spotify ID](https://developer.spotify.com/documentation/web-api/#spotify-uris-and-ids)
+    for the album.
+    """
+    id: ID!
+
+    """
+    Information about the followers of the artist.
+    """
     followerCount: Int!
+
+    """
+    Images of the artist in various sizes, widest first.
+    """
+    images: [Image!]!
+
+    """
+    The name of the artist.
+    """
+    name: String!
+  }
+
+  enum AlbumGroup {
+    ALBUM
+    SINGLE
+    APPEARS_ON
+    COMPILATION
   }
 
   type Query {
-    artist(id: String!): Artist @skipAuth
+    artist(id: ID!): Artist @skipAuth
   }
 `
--- api/src/services/artists/artists.ts
+++ api/src/services/artists/artists.ts
@@ -1,4 +1,4 @@
-import type { QueryResolvers } from 'types/graphql'
+import type { ArtistRelationResolvers, QueryResolvers } from 'types/graphql'
 
 import { db } from 'src/lib/db'
 
@@ -6,4 +6,10 @@ export const artist: QueryResolvers['artist'] = ({ id }) => {
   return db.artist.findUnique({
     where: { id },
   })
+}
+
+export const Artist: ArtistRelationResolvers = {
+  images: (_, { root }) => {
+    return db.artist.findUniqueOrThrow({ where: { id: root.id } }).images()
+  },
 }
