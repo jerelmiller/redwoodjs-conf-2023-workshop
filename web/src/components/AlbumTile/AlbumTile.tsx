f50c7758f9b1186fe51c1ffb2375fe462924d2f2
Jerel Miller <jerelmiller@gmail.com>
2023-09-12 16:03:46 -0600

    Create an AlbumTile component

---
+++ web/src/components/AlbumTile/AlbumTile.tsx
@@ -0,0 +1,58 @@
+import { ReactElement, ReactNode, forwardRef, cloneElement } from 'react'
+
+import { gql } from '@apollo/client'
+import cx from 'classnames'
+
+import { AlbumTile_album as Album } from 'types/graphql'
+import { Link } from '@redwoodjs/router'
+
+import { fragments } from 'src/apollo/fragmentRegistry'
+import CoverPhoto from 'src/components/CoverPhoto'
+import DelimitedList from 'src/components/DelimitedList'
+
+import MediaTile from 'src/components/MediaTile'
+import MediaTileCoverPhoto from 'src/components/MediaTileCoverPhoto'
+import MediaTileTitle from 'src/components/MediaTileTitle'
+import MediaTileDetails from 'src/components/MediaTileDetails'
+import { yearOfRelease } from 'src/utils/releaseDate'
+import { capitalize } from 'src/utils/string'
+
+interface AlbumTileProps {
+  album: Album
+}
+
+fragments.register(gql`
+  fragment AlbumTile_album on Album {
+    id
+    name
+    albumType
+    releaseDate {
+      date
+    }
+    images {
+      url
+    }
+    tracks {
+      pageInfo {
+        total
+      }
+    }
+  }
+`)
+
+const AlbumTile = ({ album }: AlbumTileProps) => {
+  return (
+    <MediaTile to="/albums/id">
+      <MediaTileCoverPhoto image={album.images[0]} />
+      <div className="flex flex-col">
+        <MediaTileTitle>{album.name}</MediaTileTitle>
+        <MediaTileDetails>
+          <span>{yearOfRelease(album.releaseDate)}</span>
+          <span>{capitalize(album.albumType.toLowerCase())}</span>
+        </MediaTileDetails>
+      </div>
+    </MediaTile>
+  )
+}
+
+export default AlbumTile
