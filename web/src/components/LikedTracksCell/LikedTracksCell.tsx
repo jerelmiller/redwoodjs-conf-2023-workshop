8106e3c9bb98135cc58717d0d8ffe7c341a11563
Jerel Miller <jerelmiller@gmail.com>
2023-09-13 14:13:40 -0600

    Start adding table for liked tracks

--- web/src/components/LikedTracksCell/LikedTracksCell.tsx
+++ web/src/components/LikedTracksCell/LikedTracksCell.tsx
@@ -1,3 +1,5 @@
+import { createColumnHelper } from '@tanstack/react-table'
+import { Clock } from 'lucide-react'
 import type { LikedTracksQuery } from 'types/graphql'
 
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
@@ -7,11 +9,16 @@ import PageHeader from 'src/components/PageHeader'
 import PageHeaderContent from 'src/components/PageHeaderContent'
 import PageMediaType from 'src/components/PageMediaType'
 
+import DateTime from '../DateTime'
+import Duration from '../Duration'
+import LikeButton from '../LikeButton/LikeButton'
 import LikedTracksCoverPhoto from '../LikedTracksCoverPhoto/LikedTracksCoverPhoto'
 import PageContent from '../PageContent'
 import PageHeaderDetails from '../PageHeaderDetails/PageHeaderDetails'
 import PageTitle from '../PageTitle'
 import PlayButton from '../PlayButton/PlayButton'
+import Table from '../Table'
+import TrackNumberColumn from '../TrackNumberColumn/TrackNumberColumn'
 
 export const QUERY = gql`
   query LikedTracksQuery {
@@ -19,6 +26,16 @@ export const QUERY = gql`
       profile {
         id
         displayName
+      }
+      tracks {
+        edges {
+          addedAt
+          node {
+            id
+            durationMs
+            name
+          }
+        }
       }
     }
   }
@@ -33,6 +50,8 @@ export const Failure = ({ error }: CellFailureProps) => (
 )
 
 export const Success = ({ me }: CellSuccessProps<LikedTracksQuery>) => {
+  const spotifyURI = `spotify:user:${me.profile?.id}:collection`
+
   return (
     <PageContainer bgColor="#1F3363">
       <PageHeader>
@@ -54,7 +73,90 @@ export const Success = ({ me }: CellSuccessProps<LikedTracksQuery>) => {
         <div>
           <PlayButton playing={false} size="3.5rem" variant="primary" />
         </div>
+        <Table
+          data={me.tracks?.edges ?? []}
+          columns={columns}
+          meta={{ spotifyURI } satisfies LikedTracksTableMeta}
+        />
       </PageContent>
     </PageContainer>
   )
 }
+
+type SavedTrackEdge = NonNullable<
+  NonNullable<LikedTracksQuery['me']>['tracks']
+>['edges'][0]
+
+interface LikedTracksTableMeta {
+  spotifyURI: string
+}
+
+const columnHelper = createColumnHelper<SavedTrackEdge>()
+
+const columns = [
+  columnHelper.accessor('node', {
+    id: 'number',
+    header: '#',
+    cell: (info) => {
+      const { spotifyURI } = info.table.options
+        .meta as unknown as LikedTracksTableMeta
+
+      return <TrackNumberColumn trackNumber={info.row.index + 1} />
+      // return (
+      //   <TrackNumberCell
+      //     position={info.row.index}
+      //     track={info.getValue()}
+      //     context={{
+      //       __typename: 'Playlist',
+      //       uri: spotifyURI,
+      //     }}
+      //   />
+      // )
+    },
+    meta: {
+      headerAlign: 'right',
+      shrink: true,
+    },
+  }),
+  columnHelper.accessor('node', {
+    id: 'title',
+    header: 'Title',
+    cell: (info) => {
+      const { spotifyURI } = info.table.options
+        .meta as unknown as LikedTracksTableMeta
+
+      return null
+      // return (
+      //   <TrackTitleCell context={{ uri: spotifyURI }} track={info.getValue()} />
+      // )
+    },
+  }),
+  columnHelper.accessor('addedAt', {
+    header: 'Date added',
+    cell: (info) => (
+      <DateTime date={info.getValue()} format={DateTime.FORMAT.timeAgo} />
+    ),
+  }),
+  columnHelper.accessor('node', {
+    id: 'likeButton',
+    header: '',
+    cell: () => {
+      return (
+        <div className="px-2">
+          <LikeButton liked size="1rem" className="relative top-[2px]" />
+        </div>
+      )
+    },
+    meta: {
+      shrink: true,
+    },
+  }),
+  columnHelper.accessor('node.durationMs', {
+    header: () => <Clock size="1rem" />,
+    cell: (info) => <Duration durationMs={info.getValue()} />,
+    meta: {
+      headerAlign: 'right',
+      shrink: true,
+    },
+  }),
+]
