cadbf4c44535b50948fd0af8f822a85276dde552
Jerel Miller <jerelmiller@gmail.com>
2023-09-13 17:38:59 -0600

    Add a query to get albums

--- api/src/graphql/albums.sdl.ts
+++ api/src/graphql/albums.sdl.ts
@@ -53,6 +53,19 @@ export const schema = gql`
     ): AlbumTrackConnection
   }
 
+  type AlbumConnection {
+    "The set of albums."
+    edges: [AlbumEdge!]!
+
+    "Pagination information for the set of albums."
+    pageInfo: PageInfo!
+  }
+
+  type AlbumEdge {
+    "The album."
+    node: Album!
+  }
+
   type AlbumTrackConnection {
     "The set of tracks."
     edges: [AlbumTrackEdge!]!
@@ -73,6 +86,26 @@ export const schema = gql`
   }
 
   type Query {
+    """
+    Get an album by ID.
+    """
     album(id: ID!): Album @requireAuth
+
+    """
+    Get a list of albums.
+    """
+    albums(
+      """
+      The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
+      """
+      limit: Int = 20
+
+      """
+      The index of the first album to return. Default: 0 (the first object).
+
+      Use with \`limit\` to get the next set of albums.
+      """
+      offset: Int = 0
+    ): AlbumConnection
   }
 `
