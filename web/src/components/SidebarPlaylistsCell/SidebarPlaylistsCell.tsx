0102b2187d0b2d4e1c509148a4b53f2168e909a9
Jerel Miller <jerelmiller@gmail.com>
2023-09-13 13:26:31 -0600

    Extract a SidebarPlaylistContent component

--- web/src/components/SidebarPlaylistsCell/SidebarPlaylistsCell.tsx
+++ web/src/components/SidebarPlaylistsCell/SidebarPlaylistsCell.tsx
@@ -2,13 +2,16 @@ import cx from 'classnames'
 import { Pin, Volume2 } from 'lucide-react'
 import type { SidebarPlaylistsQuery } from 'types/graphql'
 
-import { NavLink, routes } from '@redwoodjs/router'
+import { routes } from '@redwoodjs/router'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 import CoverPhoto from 'src/components/CoverPhoto'
 import DelimitedList from 'src/components/DelimitedList'
 import Skeleton from 'src/components/Skeleton'
 import { randomBetween, range } from 'src/utils/common'
+
+import SidebarPlaylistContent from '../SidebarPlaylistContent/SidebarPlaylistContent'
+import SidebarPlaylistLink from '../SidebarPlaylistLink/SidebarPlaylistLink'
 
 export const QUERY = gql`
   query SidebarPlaylistsQuery {
@@ -62,43 +65,40 @@ export const Success = ({ me }: CellSuccessProps<SidebarPlaylistsQuery>) => {
 
   return me?.playlists?.edges?.map(({ node: playlist }) => {
     return (
-      <li key={playlist.id}>
-        <NavLink
-          to={routes.playlist({ id: playlist.id })}
-          className="transition-color flex items-center justify-between gap-3 rounded-md py-2 pl-2 pr-4 leading-none transition-colors duration-200 ease-out hover:bg-[#1A1A1A] hover:no-underline"
-          activeClassName="bg-surface text-primary hover:bg-[#393939]"
-        >
-          <CoverPhoto image={playlist.images.at(-1)} size="48px" />
-          <div className="flex flex-1 flex-col justify-around self-stretch overflow-hidden text-ellipsis whitespace-nowrap">
-            <div
-              className={cx('overflow-hidden text-ellipsis whitespace-nowrap', {
-                'text-theme-light': isCurrentContext,
-              })}
-            >
-              {playlist.name}
-            </div>
-            <div className="flex items-center gap-2">
-              {pinned && (
-                <Pin
-                  fill="currentColor"
-                  size="1rem"
-                  strokeWidth={1}
-                  className="rotate-45 text-theme-light"
-                />
-              )}
-              <span className="text-sm text-muted">
-                <DelimitedList delimiter=" · ">
-                  <span>Playlist</span>
-                  <span>{playlist.owner.displayName}</span>
-                </DelimitedList>
-              </span>
-            </div>
+      <SidebarPlaylistLink
+        key={playlist.id}
+        to={routes.playlist({ id: playlist.id })}
+      >
+        <CoverPhoto image={playlist.images.at(-1)} size="48px" />
+        <SidebarPlaylistContent>
+          <div
+            className={cx('overflow-hidden text-ellipsis whitespace-nowrap', {
+              'text-theme-light': isCurrentContext,
+            })}
+          >
+            {playlist.name}
           </div>
-          {isCurrentContext && playbackState?.isPlaying && (
-            <Volume2 color="var(--color--theme--light)" size="0.875rem" />
-          )}
-        </NavLink>
-      </li>
+          <div className="flex items-center gap-2">
+            {pinned && (
+              <Pin
+                fill="currentColor"
+                size="1rem"
+                strokeWidth={1}
+                className="rotate-45 text-theme-light"
+              />
+            )}
+            <span className="text-sm text-muted">
+              <DelimitedList delimiter=" · ">
+                <span>Playlist</span>
+                <span>{playlist.owner.displayName}</span>
+              </DelimitedList>
+            </span>
+          </div>
+        </SidebarPlaylistContent>
+        {isCurrentContext && playbackState?.isPlaying && (
+          <Volume2 color="var(--color--theme--light)" size="0.875rem" />
+        )}
+      </SidebarPlaylistLink>
     )
   })
 }
