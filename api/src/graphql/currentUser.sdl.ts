d46501fffaf1188e1fd295e2f4c4517da6a755e7
Jerel Miller <jerelmiller@gmail.com>
2023-09-13 14:04:19 -0600

    Add ability to query saved tracks for a user

--- api/src/graphql/currentUser.sdl.ts
+++ api/src/graphql/currentUser.sdl.ts
@@ -14,6 +14,40 @@ export const schema = gql`
       """
       offset: Int = 0
     ): PlaylistConnection
+
+    tracks(
+      """
+      The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
+      """
+      limit: Int = 20
+
+      """
+      The index of the first track to return. Default: 0 (the first object).
+
+      Use with \`limit\` to get the next set of tracks.
+      """
+      offset: Int = 0
+    ): SavedTracksConnection
+  }
+
+  type SavedTrackEdge {
+    "The date the track was saved."
+    addedAt: DateTime!
+
+    "The track"
+    node: Track!
+  }
+
+  type SavedTracksConnection {
+    """
+    A list of saved tracks.
+    """
+    edges: [SavedTrackEdge!]!
+
+    """
+    "Pagination information for the set of playlists"
+    """
+    pageInfo: PageInfo!
   }
 
   type Query {
--- api/src/services/currentUser/currentUser.ts
+++ api/src/services/currentUser/currentUser.ts
@@ -6,10 +6,10 @@ export const me: QueryResolvers['me'] = () => {
   return context.currentUser ?? null
 }
 
+// We can safely assume we have a current user in these resolvers since the `me`
+// field returns `null` when the user is not logged in
 export const CurrentUser: CurrentUserResolvers = {
   playlists: async ({ limit, offset }) => {
-    // We can safely assume we have a current user since the `me` field returns
-    // `null` otherwise
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const currentUser = context.currentUser!
     const total = await db.playlist.count({ where: { userId: currentUser.id } })
@@ -31,4 +31,33 @@ export const CurrentUser: CurrentUserResolvers = {
     }
   },
   profile: () => context.currentUser ?? null,
+  tracks: async ({ limit, offset }) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const currentUser = context.currentUser!
+    const total = await db.savedTrack.count({
+      where: { userId: currentUser.id },
+    })
+    const savedTracks = await db.savedTrack.findMany({
+      where: {
+        userId: currentUser.id,
+      },
+      include: {
+        track: true,
+      },
+    })
+
+    return {
+      pageInfo: {
+        limit,
+        offset,
+        total,
+        hasNextPage: false,
+        hasPreviousPage: false,
+      },
+      edges: savedTracks.map((savedTrack) => ({
+        addedAt: savedTrack.addedAt,
+        node: savedTrack.track,
+      })),
+    }
+  },
 }
