52c5bd8b3153cfa64c72a6dc771927fe8d4107c7
Jerel Miller <jerelmiller@gmail.com>
2023-09-13 16:06:52 -0600

    Add primitive playbar to layout

--- web/src/layouts/BaseLayout/BaseLayout.tsx
+++ web/src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,8 +1,9 @@
 import { Home, Library, Pin } from 'lucide-react'
 
-import { Link, NavLink, routes } from '@redwoodjs/router'
+import { Link, routes } from '@redwoodjs/router'
 
 import ApolloLogo from 'src/components/ApolloLogo'
+import CoverPhoto from 'src/components/CoverPhoto'
 import CurrentUserMenuCell from 'src/components/CurrentUserMenuCell'
 import DelimitedList from 'src/components/DelimitedList'
 import LikedTracksCoverPhoto from 'src/components/LikedTracksCoverPhoto/LikedTracksCoverPhoto'
@@ -79,6 +80,119 @@ const BaseLayout = ({ children }: BaseLayoutProps) => {
           {children}
         </ScrollableList>
       </main>
+      <footer className="flex flex-col [grid-area:playbar]">
+        <div className="grid grid-cols-[30%_1fr_30%] items-center px-6 py-4 text-primary">
+          <div className="flex items-center gap-4">
+            <CoverPhoto size="4rem" image={undefined} />
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center justify-center gap-5">
+              <span>shuffle</span>
+              <span>backward</span>
+              <span>play</span>
+              <span>forward</span>
+              <span>repeat</span>
+            </div>
+          </div>
+          <div className="flex items-center justify-end gap-4">
+            <span>queue</span>
+            <span>devices</span>
+            <div className="flex items-center gap-1">
+              <span>mute</span>
+              <span>volume bar</span>
+            </div>
+          </div>
+          {/* <Flex gap="1rem" alignItems="center">
+            <CoverPhoto size="4rem" image={coverPhoto} />
+            {playbackItem?.__typename === 'Track' ? (
+              <TrackPlaybackDetails
+                context={playbackState?.context ?? null}
+                track={playbackItem}
+              />
+            ) : playbackItem?.__typename === 'Episode' ? (
+              <EpisodePlaybackDetails episode={playbackItem} />
+            ) : null}
+            {playbackState && (
+              <LikeControl playbackItem={playbackItem} size="1.25rem" />
+            )}
+          </Flex>
+          <Flex direction="column" gap="0.5rem">
+            <Flex alignItems="center" gap="1.25rem" justifyContent="center">
+              {playbackItem?.__typename === 'Track' && (
+                <ShufflePlaybackControl
+                  shuffled={playbackState?.shuffleState ?? false}
+                  disallowed={disallowed(Action.TogglingShuffle)}
+                  size="1.25rem"
+                />
+              )}
+              {playbackItem?.__typename === 'Episode' && (
+                <SkipBackwardControl
+                  ms={EPISODE_SKIP_FORWARD_AMOUNT}
+                  progressMs={playbackState?.progressMs ?? 0}
+                />
+              )}
+              <SkipToPreviousControl
+                disallowed={disallowed(Action.SkippingPrev)}
+                progressMs={playbackState?.progressMs ?? 0}
+              />
+              <PlayButton
+                disabled={!device}
+                size="2.5rem"
+                playing={playbackState?.isPlaying ?? false}
+                onPlay={() => resumePlayback()}
+              />
+              <SkipToNextControl disallowed={disallowed(Action.SkippingNext)} />
+              {playbackItem?.__typename === 'Episode' && (
+                <SkipForwardControl
+                  ms={EPISODE_SKIP_FORWARD_AMOUNT}
+                  progressMs={playbackState?.progressMs ?? 0}
+                />
+              )}
+              {playbackItem?.__typename === 'Track' && (
+                <RepeatControl
+                  disallowed={disallowed(Action.TogglingRepeatTrack)}
+                  repeatState={playbackState?.repeatState ?? RepeatMode.Off}
+                />
+              )}
+            </Flex>
+            <PlaybackItemProgressBar playbackState={playbackState} />
+          </Flex>
+          <Flex justifyContent="end" gap="1rem" alignItems="center">
+            <QueueControlButton />
+            <DevicePopover devices={devices}>
+              <PlaybarControlButton
+                disallowed={devices.length === 0}
+                icon={<DeviceIcon device={device} strokeWidth={1.5} />}
+                tooltip="Connect to a device"
+              />
+            </DevicePopover>
+            <Flex gap="0.25rem" alignItems="center">
+              <MuteControl
+                disallowed={!device}
+                volumePercent={device?.volumePercent ?? 0}
+              />
+              <VolumeBar
+                volumePercent={device?.volumePercent ?? 0}
+                width="100px"
+              />
+            </Flex>
+          </Flex> */}
+        </div>
+        {/* device && (
+          <Flex
+            alignItems="center"
+            className={cx(
+              'before:[--arrow-size:0.625rem]',
+              'border-solid before:border-b-green before:border-l-transparent before:border-r-transparent before:[border-bottom-width:var(--arrow-size)] before:[border-left-width:var(--arrow-size)] before:[border-right-width:var(--arrow-size)]',
+              'relative rounded bg-green px-6 py-1 text-sm leading-none',
+              'pointer-events-none before:absolute before:right-[10.5rem] before:top-0 before:-translate-y-full'
+            )}
+            justifyContent="end"
+          >
+            <Volume1 size="1.125rem" /> Listening on {device.name}
+          </Flex>
+        ) */}
+      </footer>
     </div>
   )
 }
