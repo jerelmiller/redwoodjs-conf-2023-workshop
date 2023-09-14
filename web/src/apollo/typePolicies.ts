91685df354d0bf5a97caa25e299849d1a80462c9
Jerel Miller <jerelmiller@gmail.com>
2023-09-14 11:48:13 -0600

    Add singleton types for current user, player, and playback state

--- web/src/apollo/typePolicies.ts
+++ web/src/apollo/typePolicies.ts
@@ -1,8 +1,14 @@
 import type { TypePolicies } from '@apollo/client'
 
 export const typePolicies: TypePolicies = {
+  CurrentUser: {
+    keyFields: [],
+  },
   Image: {
     keyFields: ['url'],
+  },
+  Player: {
+    keyFields: [],
   },
   PlaybackState: {
     keyFields: [],
