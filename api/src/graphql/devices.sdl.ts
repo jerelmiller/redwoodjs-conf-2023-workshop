a28bade8607aa613b35fe155b3031d22de2c3847
Jerel Miller <jerelmiller@gmail.com>
2023-09-14 14:23:30 -0600

    Add isActive field to device

--- api/src/graphql/devices.sdl.ts
+++ api/src/graphql/devices.sdl.ts
@@ -1,8 +1,26 @@
 export const schema = gql`
   type Device {
+    "The device ID"
     id: ID!
+
+    "If this device is the currently active device."
+    isActive: Boolean!
+
+    """
+    A human-readable name for the device.
+    """
     name: String!
+
+    """
+    Device type, such as "computer", "smartphone" or "speaker".
+    """
     type: String!
+
+    """
+    The current volume in percent.
+
+    >= 0    <= 100
+    """
     volumePercent: Int!
   }
 `
