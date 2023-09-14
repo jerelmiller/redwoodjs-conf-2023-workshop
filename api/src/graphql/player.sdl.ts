5a600caf205535be7347a6207fae428e4b9182fb
Jerel Miller <jerelmiller@gmail.com>
2023-09-14 14:24:56 -0600

    Move resume/pause playback mutations over to player

--- api/src/graphql/player.sdl.ts
+++ api/src/graphql/player.sdl.ts
@@ -9,4 +9,42 @@ export const schema = gql`
     """
     playbackState: PlaybackState
   }
+
+  input ResumePlaybackInput {
+    """
+    Spotify URI of the context to play. Valid contexts are albums, artists &
+    playlists.
+    """
+    contextUri: String
+
+    """
+    An array of the Spotify track URIs to play.
+    """
+    uris: [String!]
+  }
+
+  type ResumePlaybackPayload {
+    "The playback state after resuming playback."
+    player: Player
+  }
+
+  type PausePlaybackResponse {
+    """
+    The updated playback state
+    """
+    player: Player
+  }
+
+  type Mutation {
+    """
+    Pause playback on the user's account.
+    """
+    pausePlayback: PausePlaybackResponse @requireAuth
+
+    """
+    Start a new context or resume current playback on the user's active device.
+    """
+    resumePlayback(input: ResumePlaybackInput): ResumePlaybackPayload
+      @requireAuth
+  }
 `
--- api/src/services/player/player.ts
+++ api/src/services/player/player.ts
@@ -1,6 +1,72 @@
-import { PlayerRelationResolvers } from 'types/graphql'
+import { PlayerRelationResolvers, MutationResolvers } from 'types/graphql'
+
+import { UserInputError } from '@redwoodjs/graphql-server'
 
 import { db } from 'src/lib/db'
+
+export const resumePlayback: MutationResolvers['resumePlayback'] = async ({
+  input: _input,
+}) => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const currentUser = context.currentUser!
+  const device = await db.device.findFirst({
+    where: { userId: currentUser.id },
+  })
+
+  if (!device) {
+    throw new UserInputError('No active device found.')
+  }
+
+  await db.device.update({
+    where: { id: device.id },
+    data: {
+      isActive: true,
+    },
+  })
+
+  await db.playbackState.upsert({
+    where: { userId: currentUser.id },
+    create: {
+      isPlaying: true,
+      user: {
+        connect: { id: currentUser.id },
+      },
+      device: {
+        connect: { id: device.id },
+      },
+    },
+    update: {
+      isPlaying: true,
+    },
+  })
+
+  return {
+    player: {},
+  }
+}
+
+export const pausePlayback: MutationResolvers['pausePlayback'] = async () => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const currentUser = context.currentUser!
+  const exists = await db.playbackState
+    .findFirst({ where: { userId: currentUser.id } })
+    .then(Boolean)
+
+  if (!exists) {
+    throw new UserInputError('You are not playing anything')
+  }
+
+  await db.playbackState.update({
+    where: { userId: currentUser.id },
+    data: {
+      isPlaying: false,
+    },
+  })
+
+  return {
+    player: {},
+  }
+}
 
 export const Player: PlayerRelationResolvers = {
   devices: () => {
