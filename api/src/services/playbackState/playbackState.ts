2985ef3565d1dca9b3ffea9c968589a45c49897e
Jerel Miller <jerelmiller@gmail.com>
2023-09-14 14:57:06 -0600

    Add timestamp resolver for PlaybackState

--- api/src/services/playbackState/playbackState.ts
+++ api/src/services/playbackState/playbackState.ts
@@ -2,4 +2,6 @@ import { PlaybackStateRelationResolvers, RepeatMode } from 'types/graphql'
 
 export const PlaybackState: PlaybackStateRelationResolvers = {
   repeatState: (_, { root }) => root.repeatMode as RepeatMode,
+  shuffleState: (_, { root }) => root.shuffled,
+  timestamp: () => new Date(),
 }
