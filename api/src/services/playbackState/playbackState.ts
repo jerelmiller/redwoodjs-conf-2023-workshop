24a6c456c0592c24939542277e527acaf7730d2d
Jerel Miller <jerelmiller@gmail.com>
2023-09-14 16:39:21 -0600

    Add resolvers to handle playback state

--- api/src/services/playbackState/playbackState.ts
+++ api/src/services/playbackState/playbackState.ts
@@ -1,8 +1,14 @@
-import { PlaybackStateRelationResolvers, RepeatMode } from 'types/graphql'
+import {
+  PlaybackStateContextRelationResolvers,
+  PlaybackStateContextType,
+  PlaybackStateRelationResolvers,
+  RepeatMode,
+} from 'types/graphql'
 
 import { db } from 'src/lib/db'
 
 export const PlaybackState: PlaybackStateRelationResolvers = {
+  context: (_, { root }) => root.contextUri,
   device: async (_, { root }) => {
     return db.playbackState
       .findUniqueOrThrow({ where: { id: root.id } })
@@ -11,4 +17,13 @@ export const PlaybackState: PlaybackStateRelationResolvers = {
   repeatState: (_, { root }) => root.repeatMode as RepeatMode,
   shuffleState: (_, { root }) => root.shuffled,
   timestamp: () => new Date(),
+}
+
+export const PlaybackStateContext: PlaybackStateContextRelationResolvers = {
+  type: (_, { root }) => {
+    const [type] = root.split(':')
+
+    return type.toUpperCase() as PlaybackStateContextType
+  },
+  uri: (_, { root }) => root,
 }
