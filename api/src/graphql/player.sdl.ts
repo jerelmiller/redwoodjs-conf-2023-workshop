e55abc84451c8fa8d549548885591207ecfdd4c8
Jerel Miller <jerelmiller@gmail.com>
2023-09-14 12:30:28 -0600

    Add ability to get devices from the current user

--- api/src/graphql/player.sdl.ts
+++ api/src/graphql/player.sdl.ts
@@ -1,5 +1,8 @@
 export const schema = gql`
   type Player {
+    "Information about a user's available devices."
+    devices: [Device!]!
+
     """
     Information about the user's current playback state, including track,
     progress, and active device.
