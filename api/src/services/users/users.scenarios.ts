f49b18efee0527118b3806b681d24d974b059da5
Jerel Miller <jerelmiller@gmail.com>
2023-09-15 13:11:48 -0600

    Remove test and scenarios files from services dir

--- api/src/services/users/users.scenarios.ts
+++
@@ -1,9 +0,0 @@
-import type { Prisma, User } from '@prisma/client'
-
-import type { ScenarioData } from '@redwoodjs/testing/api'
-
-export const standard = defineScenario<Prisma.UserCreateArgs>({
-  user: { one: { data: {} }, two: { data: {} } },
-})
-
-export type StandardScenario = ScenarioData<User, 'user'>
