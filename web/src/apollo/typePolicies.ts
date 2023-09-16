1395b9ad9d89735b8fff097413eeb7bb1b7fbdf9
Jerel Miller <jerelmiller@gmail.com>
2023-09-15 18:07:22 -0600

    More robust type policy

--- web/src/apollo/typePolicies.ts
+++ web/src/apollo/typePolicies.ts
@@ -20,8 +20,20 @@ export const typePolicies: TypePolicies = {
   PlaylistTrackConnection: {
     fields: {
       edges: {
-        merge(existing = [], incoming = []) {
-          return [...existing, ...incoming]
+        merge(existing = [], incoming = [], { args }) {
+          const edges = existing.slice(0)
+
+          if (args) {
+            const { offset = 0 } = args
+
+            for (let i = 0; i < incoming.length; i++) {
+              edges[offset + i] = incoming[i]
+            }
+          } else {
+            edges.push(...incoming)
+          }
+
+          return edges
         },
       },
     },
