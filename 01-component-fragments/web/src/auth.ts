9c2b69ed4e82458c82af1cf9d9809898014c7422
Jerel Miller <jerelmiller@gmail.com>
2023-09-19 01:50:43 -0600

    Reorganize auth module a bit

--- 01-component-fragments/web/src/auth.ts
+++ 01-component-fragments/web/src/auth.ts
@@ -2,37 +2,6 @@ import { createAuthentication } from '@redwoodjs/auth'
 
 const TOKEN_CACHE_TIME = 5000
 
-let getTokenPromise: Promise<string | null> | null = null
-let lastCheckedAt: Date = new Date('1970-01-01T00:00:00')
-let cachedToken: string | null = null
-
-const isTokenCacheExpired = () => {
-  const now = new Date()
-  return now.getTime() - lastCheckedAt.getTime() > TOKEN_CACHE_TIME
-}
-
-// If you're integrating with an auth service provider you should delete this interface.
-// Instead you should import the type from their auth client sdk.
-export interface AuthClient {
-  login: () => Promise<User>
-  logout: () => Promise<boolean>
-  signup: () => never
-  getToken: () => Promise<string | null>
-  getUserMetadata: () => Promise<string | null>
-}
-
-interface User {
-  id: string
-  displayName: string
-}
-
-// If you're integrating with an auth service provider you should delete this interface
-// This type should be inferred from the general interface above
-export interface ValidateResetTokenResponse {
-  error?: string
-  [key: string]: string | undefined
-}
-
 declare global {
   /**
    * URL or absolute path to serverless functions, without the trailing slash.
@@ -43,35 +12,42 @@ declare global {
 
 const getAuthUrl = () => `${RWJS_API_URL}/auth`
 
-const getToken = async () => {
-  if (getTokenPromise) {
-    return getTokenPromise
+function createAuthClient() {
+  let getTokenPromise: Promise<string | null> | null = null
+  let lastCheckedAt = new Date('1970-01-01T00:00:00')
+  let cachedToken: string | null = null
+
+  const isTokenCacheExpired = () => {
+    const now = new Date()
+    return now.getTime() - lastCheckedAt.getTime() > TOKEN_CACHE_TIME
   }
 
-  if (isTokenCacheExpired()) {
-    getTokenPromise = fetch(`${getAuthUrl()}?method=getToken`, {
-      credentials: 'same-origin',
-    })
-      .then((res) => res.text())
-      .then((token) => {
-        console.log('got token', token)
-        lastCheckedAt = new Date()
-        cachedToken = token.length === 0 ? null : token
+  const getToken = async () => {
+    if (getTokenPromise) {
+      return getTokenPromise
+    }
 
-        return cachedToken
+    if (isTokenCacheExpired()) {
+      getTokenPromise = fetch(`${getAuthUrl()}?method=getToken`, {
+        credentials: 'same-origin',
       })
-      .catch(() => null)
-      .finally(() => {
-        getTokenPromise = null
-      })
+        .then((res) => res.text())
+        .then((token) => {
+          lastCheckedAt = new Date()
+          cachedToken = token.length === 0 ? null : token
+
+          return cachedToken
+        })
+        .catch(() => null)
+        .finally(() => {
+          getTokenPromise = null
+        })
+    }
+
+    return cachedToken
   }
 
-  return cachedToken
-}
-
-// Replace this with the auth service provider client sdk
-const client: AuthClient = {
-  login: async () => {
+  const login = async () => {
     const res = await fetch(getAuthUrl(), {
       credentials: 'same-origin',
       method: 'POST',
@@ -80,11 +56,9 @@ const client: AuthClient = {
     })
 
     return res.json()
-  },
-  signup: () => {
-    throw new Error('Does not support signup')
-  },
-  logout: async () => {
+  }
+
+  const logout = async () => {
     await fetch(getAuthUrl(), {
       credentials: 'same-origin',
       method: 'POST',
@@ -92,49 +66,18 @@ const client: AuthClient = {
     })
 
     return true
-  },
-  getToken,
-  getUserMetadata: getToken,
-}
+  }
 
-function createAuth() {
-  const authImplementation = createAuthImplementation(client)
-
-  // You can pass custom provider hooks here if you need to as a second
-  // argument. See the Redwood framework source code for how that's used
-  return createAuthentication(authImplementation)
-}
-
-// This is where most of the integration work will take place. You should keep
-// the shape of this object (i.e. keep all the key names) but change all the
-// values/functions to use methods from the auth service provider client sdk
-// you're integrating with
-function createAuthImplementation(client: AuthClient) {
   return {
     type: 'custom-auth',
-    client,
-    login: async () => client.login(),
-    logout: async () => client.logout(),
-    getToken: async () => client.getToken(),
-    signup: () => client.signup(),
-    /**
-     * Actual user metadata might look something like this
-     * {
-     *   "id": "11111111-2222-3333-4444-5555555555555",
-     *   "aud": "authenticated",
-     *   "role": "authenticated",
-     *   "roles": ["admin"],
-     *   "email": "email@example.com",
-     *   "app_metadata": {
-     *     "provider": "email"
-     *   },
-     *   "user_metadata": null,
-     *   "created_at": "2016-05-15T19:53:12.368652374-07:00",
-     *   "updated_at": "2016-05-15T19:53:12.368652374-07:00"
-     * }
-     */
-    getUserMetadata: async () => client.getUserMetadata(),
+    login,
+    logout,
+    getToken,
+    signup: () => Promise.reject(new Error('Does not implement sign up')),
+    getUserMetadata: getToken,
   }
 }
 
-export const { AuthProvider, useAuth } = createAuth()
+const authClient = createAuthClient()
+
+export const { AuthProvider, useAuth } = createAuthentication(authClient)
