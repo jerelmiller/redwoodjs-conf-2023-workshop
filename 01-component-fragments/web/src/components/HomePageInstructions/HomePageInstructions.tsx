458bd5b43a54c88b2302102d6dc6b98c8c727720
Jerel Miller <jerelmiller@gmail.com>
2023-09-19 02:33:06 -0600

    Add some custom components for markdown on home page instructions

--- 01-component-fragments/web/src/components/HomePageInstructions/HomePageInstructions.tsx
+++ 01-component-fragments/web/src/components/HomePageInstructions/HomePageInstructions.tsx
@@ -1,13 +1,22 @@
 import Markdown from 'react-markdown'
+import { ComponentPropsWithoutRef } from 'react-markdown/lib/ast-to-react'
 
 import readme from 'src/README.md?raw'
 
+type MarkdownProps = ComponentPropsWithoutRef<typeof Markdown>
+
 const HomePageInstructions = () => {
   return (
-    <div className="mx-auto flex max-w-3xl flex-1 flex-col p-[var(--main-content--padding)]">
-      <Markdown>{readme}</Markdown>
+    <div className="mx-auto flex max-w-3xl flex-1 flex-col py-[var(--main-content--padding)]">
+      <Markdown components={components}>{readme}</Markdown>
     </div>
   )
+}
+
+const components: MarkdownProps['components'] = {
+  h1: ({ children }) => <h1 className="text-6xl">{children}</h1>,
+  h2: ({ children }) => <h1 className="text-4xl">{children}</h1>,
+  p: ({ children }) => <p className="text-xl">{children}</p>,
 }
 
 export default HomePageInstructions
