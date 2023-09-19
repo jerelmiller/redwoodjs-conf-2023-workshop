32d523dd08e8043d6d613080e999afa29ce7ce76
Jerel Miller <jerelmiller@gmail.com>
2023-09-19 02:25:08 -0600

    Render README as markdown for home page instructions

--- 01-component-fragments/web/src/components/HomePageInstructions/HomePageInstructions.tsx
+++ 01-component-fragments/web/src/components/HomePageInstructions/HomePageInstructions.tsx
@@ -1,9 +1,11 @@
+import Markdown from 'react-markdown'
+
 import readme from 'src/README.md?raw'
 
 const HomePageInstructions = () => {
   return (
     <div className="mx-auto flex max-w-3xl flex-1 flex-col p-[var(--main-content--padding)]">
-      {readme}
+      <Markdown>{readme}</Markdown>
     </div>
   )
 }
