975eb5ba80ed463401560a41db52b110df3e924d
Jerel Miller <jerelmiller@gmail.com>
2023-09-19 01:11:03 -0600

    Add log in reset

--- 01-component-fragments/scripts/reset.ts
+++ 01-component-fragments/scripts/reset.ts
@@ -5,6 +5,7 @@ import { readConfig } from './shared/readConfig'
 export default async () => {
   const config = readConfig()
 
+  console.log('Resetting playback state...')
   await db.playbackState.deleteMany({
     where: { user: { displayName: config.user.displayName } },
   })
