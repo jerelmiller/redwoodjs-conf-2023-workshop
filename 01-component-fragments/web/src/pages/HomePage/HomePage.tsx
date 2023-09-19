3664000b4fd05a0326310bf0307e497ed5498a2b
Jerel Miller <jerelmiller@gmail.com>
2023-09-19 02:23:22 -0600

    Start rendering home page instructions

--- 01-component-fragments/web/src/pages/HomePage/HomePage.tsx
+++ 01-component-fragments/web/src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import { MetaTags } from '@redwoodjs/web'
 
 import { useAuth } from 'src/auth'
 import HomeCell from 'src/components/HomeCell'
+import HomePageInstructions from 'src/components/HomePageInstructions'
 import BaseLayout from 'src/layouts/BaseLayout'
 import UnauthenticatedLayout from 'src/layouts/UnauthenticatedLayout'
 
@@ -16,7 +17,9 @@ const HomePage = () => {
           <HomeCell />
         </BaseLayout>
       ) : (
-        <UnauthenticatedLayout />
+        <UnauthenticatedLayout>
+          <HomePageInstructions />
+        </UnauthenticatedLayout>
       )}
     </>
   )
