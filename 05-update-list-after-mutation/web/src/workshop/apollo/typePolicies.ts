031ffc88afb9a4ab8f9fc1989354ce982ae33b7d
Jerel Miller <jerelmiller@gmail.com>
2023-09-21 00:19:35 -0600

    Add type policies for infinite scroll

--- 05-update-list-after-mutation/web/src/workshop/apollo/typePolicies.ts
+++ 05-update-list-after-mutation/web/src/workshop/apollo/typePolicies.ts
@@ -7,4 +7,61 @@ export const typePolicies: TypePolicies = {
   Player: {
     keyFields: [],
   },
+  Playlist: {
+    fields: {
+      tracks: {
+        keyArgs: false,
+        merge: (existing, incoming, { args, mergeObjects }) => {
+          const result = {
+            __typename: 'PlaylistTrackConnection',
+            pageInfo: mergeObjects(existing?.pageInfo ?? {}, incoming.pageInfo),
+            edges: existing?.edges.slice(0) ?? [],
+          }
+
+          // Allows for loading data on an offset that might not be the end of
+          // the list
+          if (args) {
+            const { offset = 0 } = args
+
+            for (let i = 0; i < incoming.edges.length; i++) {
+              result.edges[offset + i] = incoming.edges[i]
+            }
+          } else {
+            result.edges.push(...incoming.edges)
+          }
+
+          return result
+        },
+      },
+    },
+  },
+  CurrentUser: {
+    keyFields: [],
+    fields: {
+      tracks: {
+        keyArgs: false,
+        merge: (existing, incoming, { args, mergeObjects }) => {
+          const result = {
+            __typename: 'SavedTracksConnection',
+            pageInfo: mergeObjects(existing?.pageInfo ?? {}, incoming.pageInfo),
+            edges: existing?.edges.slice(0) ?? [],
+          }
+
+          // Allows for loading data on an offset that might not be the end of
+          // the list
+          if (args) {
+            const { offset = 0 } = args
+
+            for (let i = 0; i < incoming.edges.length; i++) {
+              result.edges[offset + i] = incoming.edges[i]
+            }
+          } else {
+            result.edges.push(...incoming.edges)
+          }
+
+          return result
+        },
+      },
+    },
+  },
 }
