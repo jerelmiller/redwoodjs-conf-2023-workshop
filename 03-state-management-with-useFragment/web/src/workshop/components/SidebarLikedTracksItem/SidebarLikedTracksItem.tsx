d3b9ea91a5e2e1b969d2bcbf85466429091df1b5
Jerel Miller <jerelmiller@gmail.com>
2023-09-21 00:02:24 -0600

    Move liked tracks item in sidebar to own component

---
+++ 03-state-management-with-useFragment/web/src/workshop/components/SidebarLikedTracksItem/SidebarLikedTracksItem.tsx
@@ -0,0 +1,44 @@
+import { Pin, Volume2 } from 'lucide-react'
+
+import { routes } from '@redwoodjs/router'
+
+import DelimitedList from 'src/components/DelimitedList'
+import LikedTracksCoverPhoto from 'src/components/LikedTracksCoverPhoto'
+import SidebarPlaylistContent from 'src/components/SidebarPlaylistContent'
+import SidebarPlaylistLink from 'src/components/SidebarPlaylistLink'
+import SidebarPlaylistName from 'src/components/SidebarPlaylistName'
+
+const SidebarLikedTracksItem = () => {
+  const isPlaying = false
+  const isCurrentContext = false
+
+  return (
+    <SidebarPlaylistLink to={routes.likedTracks()}>
+      <LikedTracksCoverPhoto iconSize="1rem" size="48px" />
+      <SidebarPlaylistContent>
+        <SidebarPlaylistName isCurrentContext={isCurrentContext}>
+          Liked Songs
+        </SidebarPlaylistName>
+        <div className="flex items-center gap-2">
+          <Pin
+            fill="currentColor"
+            size="1rem"
+            strokeWidth={1}
+            className="rotate-45 text-theme-light"
+          />
+          <span className="text-sm text-muted">
+            <DelimitedList delimiter=" · ">
+              <span>Playlist</span>
+              <span>Spotify</span>
+            </DelimitedList>
+          </span>
+        </div>
+      </SidebarPlaylistContent>
+      {isCurrentContext && isPlaying && (
+        <Volume2 color="var(--color--theme--light)" size="0.875rem" />
+      )}
+    </SidebarPlaylistLink>
+  )
+}
+
+export default SidebarLikedTracksItem
