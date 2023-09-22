0ceba1ea22d03e6da70c0041b91f45f9de8c9eab
Jerel Miller <jerelmiller@gmail.com>
2023-09-21 21:42:46 -0600

    Remove generate graphiql header module from all exercises

--- 01-component-fragments/api/src/functions/graphql.ts
+++ 01-component-fragments/api/src/functions/graphql.ts
@@ -8,13 +8,11 @@ import services from 'src/services/**/*.{js,ts}'
 
 import { authDecoder, getCurrentUser } from 'src/lib/auth'
 import { db } from 'src/lib/db'
-import generateGraphiQLHeader from 'src/lib/generateGraphiQLHeader'
 import { logger } from 'src/lib/logger'
 
 export const handler = createGraphQLHandler({
   authDecoder,
   getCurrentUser,
-  generateGraphiQLHeader,
   loggerConfig: { logger, options: {} },
   directives,
   sdls,
