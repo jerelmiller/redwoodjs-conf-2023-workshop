9135e71cd39081d6fe48906a8396793e32d0fea3
Jerel Miller <jerelmiller@gmail.com>
2023-09-21 22:22:26 -0600

    Ensure setup script copies necessary files

--- 02-reusable-components-with-fragments/scripts/setup.ts
+++ 02-reusable-components-with-fragments/scripts/setup.ts
@@ -1,8 +1,18 @@
 import fs from 'node:fs'
 import path from 'node:path'
 
+const resolveSetupDir = (pathname: string) =>
+  getRelative(path.join('../../00-setup', pathname))
+
 const getRelative = (relativePath: string) =>
   path.resolve(path.join(__dirname, relativePath))
+
+const linkToShared = (pathname: string) => {
+  const absolutePath = getRelative(pathname)
+  const setupPath = resolveSetupDir(path.relative(process.cwd(), absolutePath))
+
+  symlink(setupPath, absolutePath)
+}
 
 // Create a tmp symlink, then rename it to force symlinking and avoid errors
 // when the file or symlink already exists
@@ -14,5 +24,8 @@ const symlink = (target: string, pathname: string) => {
 }
 
 export default async () => {
+  linkToShared('../api/db/dev.db')
+  linkToShared('../workshop.config.toml')
+  linkToShared('../.env')
   symlink(getRelative('../README.md'), getRelative('../web/src/README.md'))
 }
