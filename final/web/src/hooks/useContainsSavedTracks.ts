daae8ba861ca4ea8464919937765297b867f9286
Jerel Miller <jerelmiller@gmail.com>
2023-09-25 19:48:01 -0700

    Use cache-first fetch policy for useContainsSavedTracks

--- final/web/src/hooks/useContainsSavedTracks.ts
+++ final/web/src/hooks/useContainsSavedTracks.ts
@@ -17,7 +17,11 @@ export const useContainsSavedTracks = (ids: string[]) => {
   const { data, loading } = useQuery<
     ContainsSavedTracksQuery,
     ContainsSavedTracksQueryVariables
-  >(CONTAINS_SAVED_TRACKS_QUERY, { variables: { ids }, skip: ids.length === 0 })
+  >(CONTAINS_SAVED_TRACKS_QUERY, {
+    fetchPolicy: 'cache-first',
+    variables: { ids },
+    skip: ids.length === 0,
+  })
 
   return ids.reduce((map, id, idx) => {
     // If we already have loaded some data and we start loading again, try and
